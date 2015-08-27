99671890505c594f62d96de25de9d88c03a89f01
AlexAUT <alexander.weinrauch@gmail.com>
2015-08-27 23:21:56 +0200

    Modified gradle

--- desktop/src/com/alexaut/kroniax/desktop/MyPacker.java
+++ desktop/src/com/alexaut/kroniax/desktop/MyPacker.java
@@ -1,15 +1,13 @@
 package com.alexaut.kroniax.desktop;
 
-import com.badlogic.gdx.tools.texturepacker.TexturePacker;
-import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
-
-public class MyPacker {
-    public static void main(String[] args) throws Exception {
-        Settings settings = new Settings();
-        settings.maxWidth = 1024;
-        settings.maxHeight = 1024;
-        TexturePacker.process(settings, "../images", "../android/assets/data/skins/", "menu");
-
-        // TexturePacker.process("../../images/", "/textures/", "default");
-    }
+public class MyPacker {/*
+                        * public static void main(String[] args) throws
+                        * Exception { Settings settings = new Settings();
+                        * settings.maxWidth = 1024; settings.maxHeight = 1024;
+                        * TexturePacker.process(settings, "../images",
+                        * "../android/assets/data/skins/", "menu");
+                        * 
+                        * // TexturePacker.process("../../images/",
+                        * "/textures/", "default"); }
+                        */
 }
