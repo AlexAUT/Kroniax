799cfd8895b0095d0e02ab0b27b66a386629bacd
AlexAUT <alexander.weinrauch@gmail.com>
2015-08-27 23:21:46 +0200

    Added level selection grid creation

--- core/src/com/alexaut/kroniax/menu/LevelSelectionLayer.java
+++ core/src/com/alexaut/kroniax/menu/LevelSelectionLayer.java
@@ -10,9 +10,20 @@ public class LevelSelectionLayer extends Table {
         setFillParent(true);
         setPosition(0, -720); // Shouldn't be on screen at the beginning
 
-        TextButton level1 = new TextButton("1", gui.getSkin());
-        add(level1);
+        setupLevelGrid(gui);
 
         pack(); // Don't remove!
+    }
+
+    public void setupLevelGrid(Gui gui) {
+        final int unlocked_levels = 16;
+        final int rowSize = 4;
+
+        for (int i = 1; i <= unlocked_levels; i++) {
+            TextButton bt = new TextButton(Integer.toString(i), gui.getSkin());
+            add(bt).pad(10.f);
+            if (i != 0 && i % rowSize == 0)
+                row();
+        }
     }
 }
